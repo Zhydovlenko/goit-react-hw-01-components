4f58760c997be161f269fac28c5dc2b15f6d0330
Zhydovlenko <you@example.com>
2020-05-31 17:55:50 +0300

    Updates

---
+++ precache-manifest.11323fd21e1fd1712953d83f5a589944.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30937626efcb535cea5fc648e7caa474",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "8cdf4e5f26a2e9c24695",
+    "url": "/goit-react-hw-01-components/static/css/main.adb878e3.chunk.css"
+  },
+  {
+    "revision": "f29017b74d2a9a619fdf",
+    "url": "/goit-react-hw-01-components/static/js/2.3217df56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3217df56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cdf4e5f26a2e9c24695",
+    "url": "/goit-react-hw-01-components/static/js/main.cac5825f.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b8ee546a.woff"
+  }
+]);
